b576b30d8e408b1c0753c91986b4027a4ae09a25
mpjr1 <mapg.jr@gmail.com>
2015-08-22 19:11:30 +0000

    Fixed the code and passed the tests

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -4,13 +4,13 @@ class Api::UsersController < ApiController
  
   def index
     @users = User.all
-     render json: user, each_serializer: UserSerializer 
+    render json: @users, each_serializer: UserSerializer 
    end
 
   def create
     user = User.new(user_params)
     if user.save
-      render json: user.to_json
+      render json: user
     else
       render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
     end
@@ -20,7 +20,7 @@ class Api::UsersController < ApiController
   
   
   def user_params
-    params.require(:user).permit(:username, :email, :password)
+    params.require(:user).permit(:username, :password)
   end
   
   
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150820200704) do
+ActiveRecord::Schema.define(version: 20150822174925) do
 
   create_table "items", force: :cascade do |t|
     t.string   "name"
     t.string   "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "list_id"
   end
 
   create_table "lists", force: :cascade do |t|
@@ -25,6 +26,7 @@ ActiveRecord::Schema.define(version: 20150820200704) do
     t.string   "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
