59775d1cdc5419ba8116a673d1bec32681a321e2
mpjr1 <mapg.jr@gmail.com>
2015-08-23 22:35:28 +0000

    update checkpoint

--- app/controllers/api/items_controller.rb
+++ app/controllers/api/items_controller.rb
@@ -12,11 +12,19 @@
     end
    end
  
+  def update
+    item = Item.find(params[:id])
+    if item.update(item_params)
+      render json: item
+   else
+      render json: { errors: item.errors.full_messages }, status: :unprocessable_entity
+   end
+ end
    
     private
    
    def item_params
-     params.require(:item).permit(:name, :description)
+     params.require(:item).permit(:name, :description, :completed)
    end
 
  end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,7 @@
 class Item < ActiveRecord::Base
   belongs_to :list  
+  
+  def completed
+    update_attribute(:completed, true)
+  end
 end
